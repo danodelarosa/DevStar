cee34e3bc23f233c771f69d01209ee9d74079f9a
dano <josedanielayaladelarosa@gmail.com>
2023-09-01 21:12:31 -0600

    how to web scraping linkedin for search jobs

--- dev/1_test.py
+++ dev/1_test.py
@@ -1,22 +1,44 @@
-import pytube
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-# Get the YouTube video URL
-video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+class JobResult:
+    def __init__(self, date_posted, description, company, salary, expiration_date):
+        self.date_posted = date_posted
+        self.description = description
+        self.company = company
+        self.salary = salary
+        self.expiration_date = expiration_date
 
-# Create a YouTube object and login
-youtube = pytube.YouTube(video_url)
+def scrape_jobs(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    with open("indexm.html", "w", encoding="utf-8") as f:
+        for line in soup:
+            f.write(str(line))
+    job_results = []
 
-# Get the video information
-video_info = youtube.getInfo()
+    for job in soup.find_all("div", class_="job-card__container"):
+        date_posted = datetime.strptime(job.find("span", class_="posting-date").text, "%b %d, %Y")
+        description = job.find("div", class_="job-card__description").text
+        company = job.find("div", class_="job-card__company").text
+        salary = job.find("div", class_="job-card__salary").text
+        expiration_date = datetime.strptime(job.find("span", class_="posting-expiration-date").text, "%b %d, %Y")
 
-# Get the video title
-video_title = video_info["title"]
+        job_result = JobResult(date_posted, description, company, salary, expiration_date)
+        job_results.append(job_result)
 
-# Get the video format
-video_format = "mp4"
+    return job_results
 
-# Download the video
-youtube.streams.get_by_itag(video_format).download()
+def main():
+    url = "https://www.linkedin.com/jobs/search/?keywords=python&location=United%20States"
 
-# Print the success message
-print("Successfully downloaded the video: {} in {} format.".format(video_title, video_format))
+    print("hola1")
+    job_results = scrape_jobs(url)
+    
+    print("hola2")
+    for job_result in job_results:
+        print(job_result)
+    print("hola3")
+if __name__ == "__main__":
+    main()
--- dev/2_test.py
+++ dev/2_test.py
@@ -1,14 +1,43 @@
-import pytube
+import requests
+from bs4 import BeautifulSoup
 
-def download_youtube_video(url):
-    # Create a YouTube object from the URL
-    youtube = pytube.YouTube(url)
+class JobResult:
+    def __init__(self, date_posted, description, company, salary, expiration_date):
+        self.date_posted = date_posted
+        self.description = description
+        self.company = company
+        self.salary = salary
+        self.expiration_date = expiration_date
 
-    # Get the highest resolution stream available
-    stream = youtube.streams.get_highest_resolution()
+def scrape_job_listings(search_term, location):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+    }
 
-    # Download the stream
-    stream.download()
+    params = {
+        "keywords": search_term,
+        "location": location,
+    }
 
-# Example usage
-download_youtube_video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    response = requests.get("https://www.linkedin.com/jobs/search/", headers=headers, params=params)
+    soup = BeautifulSoup(response.text, "html.parser")
+    with open("indexm.html", "w", encoding="utf-8") as f:
+        for line in soup:
+            f.write(str(line))
+    job_listings = []
+    for job_card in soup.find_all("div", class_name="jobs-search-results__card"):
+        date_posted = job_card.find("span", class_name="jobs-search-results__card-result--date").text
+        description = job_card.find("div", class_name="jobs-search-results__card-result--description").text
+        company = job_card.find("div", class_name="jobs-search-results__card-result--company").text
+        salary = job_card.find("span", class_name="jobs-search-results__card-result--salary").text
+        expiration_date = job_card.find("span", class_name="jobs-search-results__card-result--expiration").text
+
+        job_listings.append(JobResult(date_posted, description, company, salary, expiration_date))
+
+    return job_listings
+
+
+if __name__ == "__main__":
+    job_listings = scrape_job_listings("Software Engineer", "United States")
+    for job_listing in job_listings:
+        print(job_listing)
